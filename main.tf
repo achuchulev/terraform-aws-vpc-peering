d6c6c83aa9f680320821e177effcbe07151202d0
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2019-07-10 15:32:47 +0300

    update 10

---
+++ main.tf
@@ -0,0 +1,78 @@
+data "aws_caller_identity" "peer" {
+  provider = "aws.peer"
+}
+
+# Requester's side of the connection.
+resource "aws_vpc_peering_connection" "requester" {
+  count         = "${var.enabled == "true" ? 1 : 0}"
+  vpc_id        = "${join("", data.aws_vpc.requester.*.id)}"
+  peer_vpc_id   = "${join("", data.aws_vpc.accepter.*.id)}"
+  peer_owner_id = "${data.aws_caller_identity.peer.account_id}"
+  peer_region   = "${var.accepter_region}"
+  auto_accept   = "false"
+  tags          = "${var.requester_peer_tags}"
+}
+
+# Accepter's side of the connection.
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  provider                  = "aws.peer"
+  count                     = "${var.enabled == "true" ? 1 : 0}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.requester.id}"
+  auto_accept               = "true"
+  tags                      = "${var.accepter_peer_tags}"
+}
+
+# Lookup requester VPC so that we can reference the CIDR
+data "aws_vpc" "requester" {
+  id = "${var.requester_vpc_id}"
+
+  #tags = "${var.requester_vpc_tags}"
+}
+
+# Lookup accepter VPC so that we can reference the CIDR
+data "aws_vpc" "accepter" {
+  provider = "aws.peer"
+  id       = "${var.accepter_vpc_id}"
+
+  #tags     = "${var.accepter_vpc_tags}"
+}
+
+# Lookup requester subnets
+data "aws_subnet_ids" "requester" {
+  vpc_id = "${data.aws_vpc.requester.id}"
+}
+
+# Lookup accepter subnets
+data "aws_subnet_ids" "accepter" {
+  provider = "aws.peer"
+  vpc_id   = "${data.aws_vpc.accepter.id}"
+}
+
+data "aws_route_tables" "requester" {
+  vpc_id = "${var.requester_vpc_id}"
+}
+
+# Lookup accepter route tables
+data "aws_route_tables" "accepter" {
+  provider = "aws.peer"
+  vpc_id   = "${var.accepter_vpc_id}"
+}
+
+# Create routes from requestor to acceptor
+resource "aws_route" "requester" {
+  count                     = "${var.enabled == "true" ? 1 : 0}"
+  route_table_id            = "${element(distinct(sort(data.aws_route_tables.requester.ids)), (ceil(count.index / (length(data.aws_vpc.accepter.*.cidr_block_associations)))))}"
+  destination_cidr_block    = "${lookup(data.aws_vpc.accepter.cidr_block_associations[count.index % (length(data.aws_vpc.accepter.*.cidr_block_associations))], "cidr_block")}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.requester.id}"
+  depends_on                = ["data.aws_route_tables.requester", "aws_vpc_peering_connection.requester"]
+}
+
+# Create routes from acceptor to requestor
+resource "aws_route" "accepter" {
+  provider                  = "aws.peer"
+  count                     = "${var.enabled == "true" ? 1 : 0}"
+  route_table_id            = "${element(distinct(sort(data.aws_route_tables.accepter.ids)), ceil(count.index / (length(data.aws_vpc.requester.*.cidr_block_associations))))}"
+  destination_cidr_block    = "${lookup(data.aws_vpc.requester.cidr_block_associations[count.index % (length(data.aws_vpc.requester.*.cidr_block_associations))], "cidr_block")}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.requester.id}"
+  depends_on                = ["data.aws_route_tables.accepter", "aws_vpc_peering_connection.requester"]
+}
---
+++ variables.tf
@@ -0,0 +1,72 @@
+variable "requester_aws_access_key" {}
+
+variable "requester_aws_secret_key" {}
+
+variable "requester_region" {}
+
+variable "accepter_aws_access_key" {}
+
+variable "accepter_aws_secret_key" {}
+
+variable "accepter_region" {}
+
+variable "enabled" {
+  description = "Set to false to prevent the module from creating or accessing any resources"
+  default     = "true"
+}
+
+variable "requester_vpc_id" {
+  type        = "string"
+  description = "Requester VPC ID"
+  default     = ""
+}
+
+variable "requester_vpc_tags" {
+  type        = "map"
+  description = "Requester VPC tags"
+
+  default = {}
+}
+
+variable "requester_peer_tags" {
+  type        = "map"
+  description = "Requester peer tags"
+
+  default = {
+    Side = "Requester"
+    Name = "VPC01-VPC02"
+  }
+}
+
+variable "accepter_peer_tags" {
+  type        = "map"
+  description = "Requester peer tags"
+
+  default = {
+    Side = "Accepter"
+    Name = "VPC01-VPC02"
+  }
+}
+
+variable "accepter_vpc_id" {
+  type        = "string"
+  description = "Acceptor VPC ID"
+  default     = ""
+}
+
+variable "accepter_vpc_tags" {
+  type        = "map"
+  description = "Accepter VPC tags"
+
+  default = {}
+}
+
+variable "requester_route_tables_count" {
+  description = "Route tables count of requester's VPC."
+  default     = "1"
+}
+
+variable "accepter_route_tables_count" {
+  description = "Route tables count of accepter's VPC."
+  default     = "1"
+}
