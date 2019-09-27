a207cd948e0ee0294ec099b6a6e2934b6e91143e
Atanas Chuchulev <42144722+achuchulev@users.noreply.github.com>
2019-09-27 18:20:40 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,9 @@
-variable "requester_aws_access_key" {
-}
-
-variable "requester_aws_secret_key" {
-}
-
-variable "requester_region" {
-}
-
-variable "accepter_aws_access_key" {
-}
-
-variable "accepter_aws_secret_key" {
-}
-
-variable "accepter_region" {
-}
+variable "requester_aws_access_key" {}
+variable "requester_aws_secret_key" {}
+variable "requester_region" {}
+variable "accepter_aws_access_key" {}
+variable "accepter_aws_secret_key" {}
+variable "accepter_region" {}
 
 variable "enabled" {
   description = "Set to false to prevent the module from creating or accessing any resources"
@@ -24,13 +13,6 @@ variable "enabled" {
 variable "requester_vpc_id" {
   type        = string
   description = "Requester VPC ID"
-}
-
-variable "requester_vpc_tags" {
-  type        = map(string)
-  description = "Requester VPC tags"
-
-  default = {}
 }
 
 variable "requester_peer_tags" {
@@ -49,7 +31,7 @@ variable "accepter_peer_tags" {
 
   default = {
     Side = "Accepter"
-    Name = "VPC01-VPC02"
+    Name = "VPC_A-VPC_B"
   }
 }
 
@@ -57,21 +39,3 @@ variable "accepter_vpc_id" {
   type        = string
   description = "Accepter VPC ID"
 }
-
-variable "accepter_vpc_tags" {
-  type        = map(string)
-  description = "Accepter VPC tags"
-
-  default = {}
-}
-
-variable "requester_route_tables_count" {
-  description = "Route tables count of requester's VPC."
-  default     = "1"
-}
-
-variable "accepter_route_tables_count" {
-  description = "Route tables count of accepter's VPC."
-  default     = "1"
-}
-
