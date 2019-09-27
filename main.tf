d70732870c677b8341a61298bba3a63a7e24cf2b
Atanas Chuchulev <42144722+achuchulev@users.noreply.github.com>
2019-09-27 18:15:21 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,50 +25,21 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
 # Lookup requester VPC so that we can reference the CIDR
 data "aws_vpc" "requester" {
   id = var.requester_vpc_id
-  #tags = "${var.requester_vpc_tags}"
 }
 
 # Lookup accepter VPC so that we can reference the CIDR
 data "aws_vpc" "accepter" {
   provider = aws.peer
   id       = var.accepter_vpc_id
-  #tags     = "${var.accepter_vpc_tags}"
-}
-
-# Lookup requester subnets
-data "aws_subnet_ids" "requester" {
-  vpc_id = data.aws_vpc.requester.id
-}
-
-# Lookup accepter subnets
-data "aws_subnet_ids" "accepter" {
-  provider = aws.peer
-  vpc_id   = data.aws_vpc.accepter.id
-}
-
-data "aws_route_tables" "requester" {
-  vpc_id = var.requester_vpc_id
-}
-
-# Lookup accepter route tables
-data "aws_route_tables" "accepter" {
-  provider = aws.peer
-  vpc_id   = var.accepter_vpc_id
 }
 
 # Create routes from requester to accepter
 resource "aws_route" "requester" {
   count = var.enabled == "true" ? 1 : 0
-  route_table_id = element(
-    distinct(sort(data.aws_route_tables.requester.ids)),
-    ceil(
-      count.index / length(data.aws_vpc.accepter.*.cidr_block_associations),
-    ),
-  )
+  route_table_id = data.aws_vpc.requester.main_route_table_id
   destination_cidr_block    = data.aws_vpc.accepter.cidr_block_associations[count.index % length(data.aws_vpc.accepter.*.cidr_block_associations)]["cidr_block"]
   vpc_peering_connection_id = aws_vpc_peering_connection.requester[count.index].id
   depends_on = [
-    data.aws_route_tables.requester,
     aws_vpc_peering_connection.requester,
   ]
 }
@@ -77,17 +48,10 @@ resource "aws_route" "requester" {
 resource "aws_route" "accepter" {
   provider = aws.peer
   count    = var.enabled == "true" ? 1 : 0
-  route_table_id = element(
-    distinct(sort(data.aws_route_tables.accepter.ids)),
-    ceil(
-      count.index / length(data.aws_vpc.requester.*.cidr_block_associations),
-    ),
-  )
+  route_table_id = data.aws_vpc.accepter.main_route_table_id
   destination_cidr_block    = data.aws_vpc.requester.cidr_block_associations[count.index % length(data.aws_vpc.requester.*.cidr_block_associations)]["cidr_block"]
   vpc_peering_connection_id = aws_vpc_peering_connection.requester[count.index].id
   depends_on = [
-    data.aws_route_tables.accepter,
-    aws_vpc_peering_connection.requester,
+    aws_vpc_peering_connection.requester
   ]
 }
-
